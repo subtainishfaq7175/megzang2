b4dd8488a9032a002fcffa4989b38e769e95ba53
Subtain Ishfaq <s.subtain@gmail.com>
2016-12-30 17:37:07 +0500

    updating product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -326,4 +326,186 @@ export class ProductComponent implements OnInit {
   var id=$(this).attr('data_id');
   $('#add_cls_'+id).remove();
 })*/;
+
+  saveLocalstoreProduct(){
+    console.log("save local store product");
+  /*if(localStorage.ProductData !=''){
+    var userData=localStorage.getItem('user');
+    var UserDataObj=JSON.parse(userData);
+    var userid={'book_by':UserDataObj.id};
+    var ObjecDat=JSON.parse(localStorage.ProductData);
+    var productdata=$.extend(userid,ObjecDat);
+    $.post('/api/Select/SaveProduct',productdata).success(function(data){
+      if(data.status=='success'){
+        setTimeout(function(){
+          $('#SetMsg').html('Your Product is Successfully created...!')
+          $('#successTogalmsg').trigger('click');
+          resetform();
+          hideproductDetails();
+          setTimeout(function(){
+            $('#closeMesg').trigger('click');
+            localStorage.setItem("TextVal",'');
+            localStorage.setItem('ProductData','');
+          },3000);
+
+        },200);
+        $('#SetOnlineLink').val('');
+      }
+
+    })
+      .error(function(data){
+        $scope.loginError = true;
+        $scope.registerErrorText = data.error;
+      });
+  }*/
+}
+  GetSetDataForurl(/*data,Prdoucturl*/){
+ /* $('#Productprice').val('')
+  $('#ProductName').val('');
+  $('#setBrace').html('0.0 bars');
+  $('#setSrcTag').attr('src','');
+  $('#setSrcTag').css('display','none');
+  setTimeout(function(){
+    var Productname=$(data).filter('title').text();
+    var arr=['Amazon.in','Amazon.com','eBay','flipkart.com','snapdeal',':','|'];
+    var FullName=Productname;
+    if(Productname){
+      for(var i=0;i<arr.length;i++){
+        FullName=FullName.replace(arr[i],'');
+      }
+      if(FullName){
+        $('#ProductName').val($.trim(FullName));
+        $('.loading').hide();
+        Showonlinedata(Prdoucturl);
+        ShowproductDetails();
+
+        ////Price Tag Find
+        var Pricarr=['#prcIsum','#priceblock_ourprice'];
+        for(var i=0;i <= Pricarr.length;i++){
+          if(Pricarr[i]){
+            if($(data).find(Pricarr[i]).text()){
+              var price=$(data).find(Pricarr[i]).text();
+              break;
+            }
+          }
+        }
+        // End Price
+
+        //Img Tag Find
+        var Imgarr=['#landingImage','#icImg'];
+        for(var i=0;i <= Imgarr.length;i++){
+          if(Imgarr[i]){
+            if($(data).find(Imgarr[i]).attr('src')){
+              var img=$(data).find(Imgarr[i]).attr('src');
+              break;
+            }
+          }
+        }
+        if(img){
+          $('#setSrcTag').css('display','block');
+          $('#setSrcTag').attr('src',img);
+        }
+        //End Img
+
+        // Clean Price
+        var CleanPricearr=['Rs.','US','$'];
+        var ProductPrice=price;
+        for(var i=0;i<CleanPricearr.length;i++){
+          ProductPrice=ProductPrice.replace(CleanPricearr[i],'');
+        }
+        $('#Productprice').val($.trim(ProductPrice));
+        //end
+        if(ProductPrice){
+          if(ProductPrice.indexOf(',') > 0){
+            var priceval = ProductPrice.replace(',','');
+            var braseVal=priceval / 5 ;
+          }else if(ProductPrice.indexOf('.') > 0){
+            var priceval = ProductPrice.replace('$','');
+            var braseVal= priceval / 5 ;
+          }else{
+            var braseVal= ProductPrice / 5 ;
+          }
+        }else{
+          var braseVal=0;
+        }
+        if(braseVal !=''){
+          $('#setBrace').html(braseVal.toFixed(2) +'  bars');
+        }else{
+          $('#setBrace').html('0.0 bars');
+        }
+
+      }else{
+        $('.loading').hide();
+      }
+    }
+  },1000);*/
+}
+  Showonlinedata(/*valurl*/){
+ /* $('#offlineStore').attr('checked',false);
+  $('#onlineStore').attr('checked',true);
+  $('#onlineStoreDetails').show();
+  $('#offlineStoreDetails').hide();
+  $('#onlineStore').prop('checked',true);
+  $('#SetOnlineLink').val(valurl);*/
+}
+  resetform(){
+
+ /* $('#PackageForm')[0].reset();
+  $('#StoreForm')[0].reset();
+  $('#ProductForm')[0].reset();
+  $('#addOption').html('');
+  prodid=1;
+
+  $('#ProductName').val('');
+  $('#SetProductEro').removeClass('success');
+  $('#SetProductEro').removeClass('error');
+  $("#SetProductEroHtml").html('<i class="material-icons md-24 align-vmid">check</i><i class="material-icons md-24 align-vmid">close</i>');
+
+  $('#Productprice').val('');
+  $('#SetPriceClass').removeClass('success');
+  $('#SetPriceClass').removeClass('error');
+  $("#SetPriceClassHtml").html('<i class="material-icons md-24 align-vmid">check</i><i class="material-icons md-24 align-vmid">close</i>');
+  $("#SetPriceClassEro").html('');
+
+  $('#setSrcTag').attr('src','');
+  $('#setSrcTag').css('display','none');
+  $('#RetailsName').val('');
+
+  $('#img_name').html('');
+  $('#upload-img').html('');
+  $('#upload-img').html('<div class="dz-default dz-message"><span>Drag and drop images to upload <br> or <br> <a class="button nrm-pad uppercase mrg-t-s">Choose images</a></span></div>');
+*/
+}
+  hideproductDetails(){
+  /*$('#Productdetails').hide();
+  $('#Packageinfo').hide();
+  $('#buttonSvae').hide();*/
+}
+  ShowproductDetails(){
+ /* $('#Productdetails').show();
+  $('#Packageinfo').show();
+  $('#buttonSvae').show();*/
+}
+  productResrt(){
+  /*$('#PackageForm')[0].reset();
+  $('#ProductForm')[0].reset();
+  $('#addOption').html('');
+  prodid=1;*/
+}
+  validationForm(){
+  /*var RetailsName=$('#RetailsName').val();
+  if($.trim(RetailsName)==''){
+    $('#Retailer_error').html('required')
+  }else{
+    $('#Retailer_error').html('')
+  }*/
+}
+  validationForm1(){
+  /*var autocomplete=$('#autocomplete').val();
+  if($.trim(autocomplete)==''){
+    $('#autocomplete_eror').html('required')
+  }else{
+    $('#autocomplete_eror').html('')
+  }*/
+}
 }
